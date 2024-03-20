e814b8305b1a7f499e5db51d828cdefdeee68a4a
pc852 <pengchen_twd@yahoo.com.sg>
2024-03-20 19:44:39 +0800

    initial commit p4
    experiments

---
+++ experiments/sushi/datasets/dataset_final_2223/asprin-vL-1.0-default/training_po_2223/user2237/val3/less_weight/training_set.lp/run1/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# http://www.cril.univ-artois.fr/~roussel/runsolver/
+
+CAT="../../../../../../../../../../../../programs/gcat.sh"
+
+cd "$(dirname $0)"
+
+#top -n 1 -b > top.txt
+
+[[ -e .finished ]] || $CAT "../../../../../../../../../../../../benchmarks/clasp/training_po_2223/user2237/val3/less_weight/training_set.lp" | "../../../../../../../../../../../../programs/runsolver-3.4.0" \
+	-M 20000 \
+	-w runsolver.watcher \
+	-o runsolver.solver \
+	-W 599 \
+	"../../../../../../../../../../../../programs/asprin-vL-1.0" --stats=1 --print_output_instances --min_element -t4
+
+touch .finished
